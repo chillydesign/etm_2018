71b8651df13fd37737cd0b151185d17bcf86fb92
Charles Harvey <harvey.charles@gmail.com>
2020-01-28 10:07:10 +0100

    event title should be link now

--- template-evenements.php
+++ template-evenements.php
@@ -34,17 +34,18 @@
 				); ?>
 							<?php while ( $actualite_loop->have_posts() ) : $actualite_loop->the_post(); ?>
 							<?php $post_id = get_the_ID(); ?>
-							<?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
+							<?php $url = wp_get_attachment_url( get_post_thumbnail_id( $post_id) ); ?>
 								<section class="event">
 									<div class="wrapper">
 										<div class="row">
 											<div class="col-md-9 col-md-push-3">
-												<h2><?php the_title(); ?></h2>
+												<?php $permalink = get_the_permalink(); ?>
+												<h2><a href="<?php echo $permalink; ?>"><?php the_title(); ?></a></h2>
 												<?php $date_field = get_field('date'); ?>
 												<?php if(!empty($date_field)){ ?><h2 style="font-weight:300; margin:-30px 0 30px; font-size:1.2em;"><?php the_field('date'); ?></h2><?php } ?>
-												<p><?php the_excerpt(); ?>&hellip; <a href="<?php the_permalink(); ?>">Lire la suite</a></p>
+												<p><?php the_excerpt(); ?>&hellip; <a href="<?php echo $permalink; ?>; ?>">Lire la suite</a></p>
 											</div>
-											<div class="col-md-3 col-md-pull-9" style="padding: 0px 60px 0 0;"><a href="<?php the_permalink(); ?>"><!-- <div style="width:100%; padding: 37%; background:url(<?php //echo $url;?>) no-repeat; background-size: cover; margin-bottom:20px; margin-top:6px;"></div> --><img src="<?php echo $url; ?>" <?php if(get_field('photo_circulaire')){?> style="border-radius:50%;" <?php } ?>></a></div>
+											<div class="col-md-3 col-md-pull-9" style="padding: 0px 60px 0 0;"><a href="<?php echo $permalink; ?>"><!-- <div style="width:100%; padding: 37%; background:url(<?php //echo $url;?>) no-repeat; background-size: cover; margin-bottom:20px; margin-top:6px;"></div> --><img src="<?php echo $url; ?>" <?php if(get_field('photo_circulaire')){?> style="border-radius:50%;" <?php } ?>></a></div>
 									 	
 										</div>
 									</div>
