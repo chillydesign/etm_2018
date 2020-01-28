befcc1a6f8e955ac8ac12cc1d32293c228cb1a86
Charles Harvey <harvey.charles@gmail.com>
2020-01-28 10:04:28 +0100

    move read more link up a line on events

--- template-evenements.php
+++ template-evenements.php
@@ -42,8 +42,7 @@
 												<h2><?php the_title(); ?></h2>
 												<?php $date_field = get_field('date'); ?>
 												<?php if(!empty($date_field)){ ?><h2 style="font-weight:300; margin:-30px 0 30px; font-size:1.2em;"><?php the_field('date'); ?></h2><?php } ?>
-												<p><?php the_excerpt(); ?>&hellip;<br/></br/>
-												<a href="<?php the_permalink(); ?>">Lire la suite</a></p>
+												<p><?php the_excerpt(); ?>&hellip; <a href="<?php the_permalink(); ?>">Lire la suite</a></p>
 											</div>
 											<div class="col-md-3 col-md-pull-9" style="padding: 0px 60px 0 0;"><a href="<?php the_permalink(); ?>"><!-- <div style="width:100%; padding: 37%; background:url(<?php //echo $url;?>) no-repeat; background-size: cover; margin-bottom:20px; margin-top:6px;"></div> --><img src="<?php echo $url; ?>" <?php if(get_field('photo_circulaire')){?> style="border-radius:50%;" <?php } ?>></a></div>
 									 	
