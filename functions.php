4c30427e3024585e1af061dbb4ce4e4562ecc23a
Charles Harvey <harvey.charles@gmail.com>
2020-06-24 07:41:58 +0200

    change thumbnail sizes

--- functions.php
+++ functions.php
@@ -25,10 +25,12 @@ if (function_exists('add_theme_support')) {
 
     // Add Thumbnail Theme Support
     add_theme_support('post-thumbnails');
-    add_image_size('large', 700, '', true); // Large Thumbnail
-    add_image_size('medium', 250, '', true); // Medium Thumbnail
-    add_image_size('small', 120, '', true); // Small Thumbnail
-    add_image_size('custom-size', 700, 200, true); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
+    add_image_size('large', 1200, '', true); // Large Thumbnail
+    add_image_size('medium', 400, '', true); // Medium Thumbnail
+    add_image_size('small', 250, '', true); // Small Thumbnail
+    add_image_size('thumbnail', 150, 150, true); // Small Thumbnail
+
+    // add_image_size('custom-size', 700, 200, true); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
 
     // Add Support for Custom Backgrounds - Uncomment below if you're going to use
     /*add_theme_support('custom-background', array(
@@ -394,7 +396,7 @@ function html5blankcomments($comment, $args, $depth)
             <div class="comment-meta commentmetadata"><a href="<?php echo htmlspecialchars(get_comment_link($comment->comment_ID)) ?>">
                     <?php
                     printf(__('%1$s at %2$s'), get_comment_date(),  get_comment_time()) ?></a><?php edit_comment_link(__('(Edit)'), '  ', '');
-                                                                                        ?>
+                                                                                                ?>
             </div>
 
             <?php comment_text() ?>
