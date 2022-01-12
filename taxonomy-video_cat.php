5af17969d4fa3b46474ee5fc1d9f62fe0ff51db6
Charles Harvey <harvey.charles@gmail.com>
2022-01-12 14:05:26 +0000

    ditto

--- taxonomy-video_cat.php
+++ taxonomy-video_cat.php
@@ -10,33 +10,32 @@
         </div>
     </section>
 
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <?php if (have_posts()) : ?>
 
 
-            <section class="video">
-                <div class="wrapper">
-                    <div class="row">
-                        <?php $i = 1; ?>
-                        <?php while ($video_loop->have_posts()) : $video_loop->the_post() ?>
-                            <?php $post_id = get_the_ID(); ?>
-                            <?php $url = wp_get_attachment_url(get_post_thumbnail_id($post->ID)); ?>
+        <section class="video">
+            <div class="wrapper">
+                <div class="row">
+                    <?php $i = 1; ?>
+                    <?php while (have_posts()) : the_post();  ?>
+                        <?php $post_id = get_the_ID(); ?>
+                        <?php $url = wp_get_attachment_url(get_post_thumbnail_id($post->ID)); ?>
 
-                            <div class="col-sm-6" style="margin-bottom:40px;">
-                                <?php the_content(); ?>
-                                <h5><?php the_title(); ?></h5>
-                            </div>
-                            <?php if ($i % 2 == 0) { ?>
-                    </div>
-                    <div class="row"> <?php } ?>
-                    <?php $i++; ?>
-                <?php endwhile; ?>
-                    </div>
-            </section>
+                        <div class="col-sm-6" style="margin-bottom:40px;">
+                            <?php the_content(); ?>
+                            <h5><?php the_title(); ?></h5>
+                        </div>
+                        <?php if ($i % 2 == 0) { ?>
+                </div>
+                <div class="row"> <?php } ?>
+                <?php $i++; ?>
+            <?php endwhile; ?>
+                </div>
+        </section>
 
 
 
-            <?php get_template_part('pagination'); ?>
-        <?php endwhile; ?>
+        <?php get_template_part('pagination'); ?>
     <?php endif; ?>
 </main>
 
