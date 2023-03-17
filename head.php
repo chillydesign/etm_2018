332a51ae383fa472b57bb507849f5d3e162c77d8
Charles Harvey <harvey.charles@gmail.com>
2023-03-17 12:31:19 +0000

    add new ema logo

--- head.php
+++ head.php
@@ -6,10 +6,10 @@
 	<title><?php wp_title(''); ?><?php if (wp_title('', false)) {
 										echo ' :';
 									} ?> <?php bloginfo('name'); ?></title>
-
+	<?php $tdu = get_template_directory_uri(); ?>
 	<link href="//www.google-analytics.com" rel="dns-prefetch">
-	<link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
-	<link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
+	<link href="<?php echo $tdu; ?>/img/ema_favicon.png" rel="shortcut icon">
+	<link href="<?php echo $tdu; ?>/img/ema_favicon.png" rel="apple-touch-icon-precomposed">
 
 
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
